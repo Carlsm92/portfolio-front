2f2bb0cc09c89a76aee254571bd036f61bf2f83f
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-10-29 13:50:21 +0100

    A - Footer & translations

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  socialLinks = [
+    {
+      link: 'https://www.facebook.com/Carlossanmil?locale=es_ES',
+      img: 'facebook',
+    },
+    {
+      link: 'https://twitter.com/Carlkazuu',
+      img: 'x',
+    },
+    {
+      link: 'https://www.instagram.com/carlsanm/',
+      img: 'instagram',
+    },
+    {
+      link: 'https://www.linkedin.com/in/carlos-s%C3%A1nchez-mil%C3%A1n-7a3748145/',
+      img: 'linkedin',
+    },
+  ];
 }

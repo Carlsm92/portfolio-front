33c539338106ebbd9c85570e5c7c5baeb940a4ea
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-10-29 18:30:22 +0100

    A - Navbar links bucle & links interface

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 
+interface Link {
+  link: string;
+  name: string;
+}
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,4 +13,19 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  links: Link[] = [
+    {
+      link: '#about',
+      name: 'header.nav.about',
+    },
+    {
+      link: '#projects',
+      name: 'header.nav.projects',
+    },
+    {
+      link: '#contact',
+      name: 'header.nav.contact',
+    },
+  ];
+}
